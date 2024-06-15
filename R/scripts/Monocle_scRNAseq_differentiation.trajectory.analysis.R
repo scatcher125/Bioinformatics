8b5d848ff45c86347a78c093eb11b02e63879e79
scatcher125 <reva.shenwai96@gmail.com>
2024-06-15 14:46:37 -0700

    - Monocle scRNAseq workflow

---
+++ R/scripts/Monocle_scRNAseq_differentiation.trajectory.analysis.R
@@ -0,0 +1,15 @@
+## Single cell RNAseq analyses Monocle
+## - Includes dimensionality reduction, clustering, cell type 
+##   identification and differentiation trajectory analysis.
+## Reva Shenwai
+## 19-May-2024
+## ---------------------------------------------------
+
+# Load libraries
+
+# Load data
+
+# Set parameters
+
+# User input is not required beyong this point
+# Analysis
